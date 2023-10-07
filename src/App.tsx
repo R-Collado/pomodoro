01324a3c1fb8560df16c2c0c197ef18e71b2c0d2
R-Collado <roger.collado1130@gmail.com>
2023-10-07 10:09:31 +0200

    fix unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react';
 import './scss/main.scss';
 
 import { Home } from './components/home/home';
 import { PrimaryNav } from './components/navigation/primary-nav/primary-nav';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
             <header>
