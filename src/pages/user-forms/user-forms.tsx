1792d4724a18d7ae32d2339598d2b8d5a252ef97
R-Collado <roger.collado1130@gmail.com>
2023-10-20 17:22:33 +0200

    comment so it builds

--- src/pages/user-forms/user-forms.tsx
+++ src/pages/user-forms/user-forms.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+// import { useCallback } from 'react';
 import { User } from '../../interfaces/user.interface';
 import { LoginForm } from './login-form/login-form';
 import { RegisterForm } from './register-form/register-form';
