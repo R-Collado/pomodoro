9fd3eb5c76343eb0506d88fd18069e87eea75d4b
R-Collado <roger.collado1130@gmail.com>
2023-10-20 17:07:07 +0200

    make icons usable as react components

---
+++ src/assets/svgs/logout.tsx
@@ -0,0 +1,18 @@
+export const LogoutIcon = () => {
+    return (
+        <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M5.48761 2.25H3.17143C2.82041 2.25 2.48376 2.38828 2.23555 2.63442C1.98734 2.88056 1.8479 3.2144 1.8479 3.5625V11.4375C1.8479 11.7856 1.98734 12.1194 2.23555 12.3656C2.48376 12.6117 2.82041 12.75 3.17143 12.75H5.48761M5.6521 7.5H13.1521M13.1521 7.5L10.2864 4.5M13.1521 7.5L10.2864 10.5"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>
+    );
+};
