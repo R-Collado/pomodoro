cc53aee0a7f1af276fe26a235f5d977a65f3c051
R-Collado <roger.collado1130@gmail.com>
2023-10-21 10:59:28 +0200

    create focus component

---
+++ src/pages/focus/focus.tsx
@@ -0,0 +1,9 @@
+export const Focus = () => {
+    return (
+        <div className="container" data-type="wide">
+            <section className="focus">
+                <h1>Hello</h1>
+            </section>
+        </div>
+    );
+};
