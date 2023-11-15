f3e837cbe4bafb0063670c9666b9d29dc6592bd7
R-Collado <roger.collado1130@gmail.com>
2023-11-15 17:13:49 +0100

    create user settings component

---
+++ src/pages/settings/settings.tsx
@@ -0,0 +1,11 @@
+import { UserSettings } from './user-settings/user-settings';
+
+export const Settings = () => {
+    return (
+        <section className="settings">
+            <div className="container">
+                <UserSettings />
+            </div>
+        </section>
+    );
+};
